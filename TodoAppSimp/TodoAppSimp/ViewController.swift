3aeb7f5f681da31a09b4b63e3e43667f71d235d2
田中 慎 <ann.raf.margiela@gmail.com>
2015-04-06 03:30:57 +0900

    edit delete method add

--- TodoAppSimp/TodoAppSimp/ViewController.swift
+++ TodoAppSimp/TodoAppSimp/ViewController.swift
@@ -17,6 +17,12 @@ class ViewController: UIViewController, UITableViewDelegate{
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // ナビゲーションバーの右側に編集ボタンを追加.
+        self.navigationItem.leftBarButtonItem = self.editButtonItem()
+
+        // テーブルのセルを編集可能に
+        tableView.allowsSelectionDuringEditing = true
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,9 +39,62 @@ class ViewController: UIViewController, UITableViewDelegate{
         }
         return cellCount
     }
+
+    // セル選択時処理
+    func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath:NSIndexPath!) {
+        println("\(indexPath.row)行")
+        println("Edeintg: \(tableView.editing)")
+    }
+
+    override func setEditing(editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        
+        tableView.setEditing(editing, animated: true)
+        
+        if editing {
+            println("編集モード")
+        } else {
+            println("編集可能")
+        }
+    }
+    
+    // 削除ボタン押下時に呼ばれる
+    func tableView(tableView: UITableView!, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath!) {
+        // 削除のとき.
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            println("削除")
+
+            if let title = defaults.objectForKey("title") as? [String]{
+                // 指定されたセルのオブジェクトをmyItemsから削除する.
+                
+                var arr:[String] = title
+                arr.removeAtIndex(indexPath.row)
+                println(arr)
+
+                // 配列に値追加
+                defaults.setObject(arr,forKey: "title")
+
+                // 同期
+                defaults.synchronize()
+            }
+
+            // 時間を配列に追加
+            if let timeStamp = defaults.objectForKey("timeStamp") as? [String]{
+                var timeStamp:[String] = timeStamp
+                timeStamp.removeAtIndex(indexPath.row)
+                
+                defaults.setObject(timeStamp, forKey: "timeStamp")
+            }
+            
+            // TableViewを再読み込み.
+            tableView.reloadData()
+        }
+    }
+    
     
     func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
-        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "myCell")
+        let cell: UITableViewCell
+        = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "myCell")
 
         let title = defaults.objectForKey("title") as? [String]
         var arr:[String]! = title
