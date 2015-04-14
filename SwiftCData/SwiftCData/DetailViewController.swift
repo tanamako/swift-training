8622b3cc9a05fa25ed1739d6349f025ecc527773
田中 慎 <ann.raf.margiela@gmail.com>
2015-04-14 11:53:53 +0900

    optional binding

--- SwiftCData/SwiftCData/DetailViewController.swift
+++ SwiftCData/SwiftCData/DetailViewController.swift
@@ -21,8 +21,8 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if todo != nil {
-            self.todoValue.text = todo?.title
+        if let todo = todo {
+            self.todoValue.text = todo.title
         }
     }
     
